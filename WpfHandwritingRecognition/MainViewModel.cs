f5b523adc1c090625aaf21a1cce3d6e5af611959
Bill Chung <bill_chungiii@hotmail.com>
2021-10-13 19:06:15 +0800

    加入專案檔案。

---
+++ WpfHandwritingRecognition/MainViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Ink;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfHandwritingRecognition
+{
+    public class MainViewModel : NotifyPropertyBase
+    {
+
+        public MainViewModel()
+        {
+            _candidates = new ObservableCollection<string>();
+            _strokes = new StrokeCollection();
+            _drawingAttributes = new System.Windows.Ink.DrawingAttributes
+            {
+                Color = Colors.DarkBlue,
+                FitToCurve = true,
+                IgnorePressure = false,
+                IsHighlighter = false,
+                StylusTip = StylusTip.Ellipse,
+                Width = 18,
+                Height = 18,
+            };
+        }
+
+        private StrokeCollection _strokes;
+
+        public StrokeCollection Strokes
+        {
+            get => _strokes;
+            set => SetProperty(ref _strokes, value);
+        }
+
+        private ObservableCollection<string> _candidates;
+
+        public ObservableCollection<string> Candidates
+        {
+            get => _candidates;
+            set => SetProperty(ref _candidates, value);
+        }
+
+        private System.Windows.Ink.DrawingAttributes _drawingAttributes;
+        public System.Windows.Ink.DrawingAttributes DrawingAttributes
+        {
+            get => _drawingAttributes;
+            set => SetProperty(ref _drawingAttributes, value);
+        }       
+
+        public ICommand Clear
+        {
+            get
+            {
+                return new RelayCommand((x) =>
+               {
+                   Strokes.Clear();
+                   Candidates.Clear();
+               });
+            }
+        }
+        public ICommand Recognize
+        {
+            get
+            {
+                return new RelayCommand((x) =>
+                {
+                    Candidates.Clear();
+                    if (Strokes.Count == 0) return;
+                    using (var stream = new MemoryStream())
+                    {
+                        Strokes.Save(stream);
+                        var ink = new Ink();
+                        ink.Load(stream.ToArray());
+                        using (var context = new RecognizerContext())
+                        {
+                            context.Strokes = ink.Strokes;
+                            var result = context.Recognize(out RecognitionStatus status);
+                            if (status == RecognitionStatus.NoError)
+                            {
+                                foreach (var candidate in result.GetAlternatesFromSelection())
+                                {
+                                    Candidates.Add(candidate.ToString());
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ WpfHandwritingRecognition/NotifyPropertyBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfHandwritingRecognition
+{
+    public abstract class NotifyPropertyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {           
+
+            if (!EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                storage = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _executeHandler;
+        private Func<object, bool> _canExecuteHandler;
+
+        public event EventHandler CanExecuteChanged;
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public RelayCommand(Action<object> executeHandler, Func<object, bool> canExecuteHandler)
+        {
+            _executeHandler = executeHandler ?? throw new ArgumentNullException("execute handler can not be null");
+            _canExecuteHandler = canExecuteHandler ?? throw new ArgumentNullException("canExecute handler can not be null");
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, (x) => true)
+        { }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteHandler(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeHandler(parameter);
+        }
+
+    }
+}
